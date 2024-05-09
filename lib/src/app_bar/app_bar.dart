90c38d6ecfc13d8c10b15b48ec61f6396491448b
Mario E. Bermonti-Pérez <mbermonti1132@gmail.com>
2024-05-09 14:02:41 -0400

    refactor: Make export button const

--- lib/src/app_bar/app_bar.dart
+++ lib/src/app_bar/app_bar.dart
@@ -4,7 +4,7 @@ import 'package:mdigit_span_tasks/src/export_data/view/export_button.dart';
 final AppBar appBar = AppBar(
   title: const Text('mDigitSpanTask'),
   centerTitle: true,
-  actions: [
+  actions: const [
     ExportButton(),
   ],
 );
